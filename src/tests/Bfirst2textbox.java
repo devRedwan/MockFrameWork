90834b2ed026cbdaa1c056e1315b30b967bde2e9
Red's Laptop <aredwan.rahmed@gmail.com>
2018-11-05 23:56:59 -0500

    tc01
    tc01 completed

--- src/tests/Bfirst2textbox.java
+++ src/tests/Bfirst2textbox.java
@@ -1,28 +1,19 @@
 package tests;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
 import base.config;
 import locators.locators;
 import values.values;
 
-public class tests extends config {
+public class Bfirst2textbox extends config {
 
 	locators loc = new locators();
 	values val = new values();
-	
-	@Test
-	public void tc01(){
-		
-		
-	}
-	
 	@Test
 	public void tc02(){
 	
-		String locfrom = placeholdtext (loc.locationfrom); // test
+		String locfrom = placeholdtext (loc.locationfrom);
 		System.out.println(locfrom);
 		String locto = placeholdtext (loc.locationto);
 		System.out.println(locto);
@@ -30,5 +21,5 @@ public class tests extends config {
 	
 		
 	}
-	
+
 }
