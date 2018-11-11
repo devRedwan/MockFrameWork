ec01d50ed14d3dcd39b6c280af467f9d5fec436d
Red's Laptop <aredwan.rahmed@gmail.com>
2018-11-11 11:06:13 -0500

    fixing tc04b

--- src/tests/Cdepart.java
+++ src/tests/Cdepart.java
@@ -11,11 +11,11 @@ public class Cdepart extends config {
 	locators loc = new locators();
 	values val = new values();
 	
-	@Test
+	@Test (enabled=false)
 	public void tc04a (){
 		//User should be able to enter integer values in the "Depart" & "Guests" text-boxes
 
-		typebyxpath (loc.departdate, val.tc03txtbxinteger);
+		typebyxpath (loc.departdate, val.tc03txtbxinteger); 
 		String locdepartint = textverification (loc.departdate);
 		System.out.println(locdepartint);
 		System.out.println(val.tc03txtbxinteger);
@@ -28,25 +28,31 @@ public class Cdepart extends config {
 		System.out.println(val.tc03txtbxinteger);
 		if (locguestsint==val.tc03txtbxinteger){System.out.println("PASS: guests textbox is active for integers");}
 		else                                   {System.out.println("FAIL: guests textbox allows integers");}
+		
 	}
 	
 	@Test
 	public void tc04b(){
 	// User should NOT be able to enter letters in the "Depart", "Guests" text-fields
-		typebyxpath (loc.departdate, val.tc03txtbxletters);
-		String locdepartltr = textverification (loc.departdate);
-		System.out.println(locdepartltr);
-		System.out.println(val.tc03txtbxletters);
-		if (locdepartltr==val.tc03txtbxletters){System.out.println("FAIL: depart textbox is active for letters");}
-		else                                   {System.out.println("PASS: depart textbox is NOT active for letters");}
-		
+//		typebyxpath (loc.departdate, val.tc03txtbxletters);
+//		String locdepartltr = textverification (loc.departdate);
+//		System.out.println(locdepartltr);
+//		System.out.println(val.tc03txtbxletters);
+//		if (locdepartltr==val.tc03txtbxletters){System.out.println("FAIL: depart textbox is active for letters");}
+//		else                                   {System.out.println("PASS: depart textbox is NOT active for letters");}
+//		
 		typebyxpath (loc.passenger, val.tc03txtbxletters);
-		String locguestsltr  = textverification (loc.passenger);
-		System.out.println(locdepartltr);
+	//	String locguestsltr  = textverification (loc.passenger);
+//		System.out.println(locdepartltr);
 		System.out.println(val.tc03txtbxletters);
-		if (locguestsltr==val.tc03txtbxletters){System.out.println("FAIL: Guests textbox allows letters");}
-		else                                   {System.out.println("PASS: Guests textbox rejects letters");}
-		
+		//if (locguestsltr==val.tc03txtbxletters){System.out.println("FAIL: Guests textbox allows letters");}
+	//	else                                   {System.out.println("PASS: Guests textbox rejects letters");}
+		if(txtverify(loc.passenger, val.tc03txtbxletters)){
+			System.out.println("FAIL: Guests textbox allows letters");
+		}
+		else{
+			System.out.println("PASS: Guests textbox rejects letters");
+		}
 	}
 	
 }
--- src/utils/webdriverfunctions.java
+++ src/utils/webdriverfunctions.java
@@ -1,12 +1,17 @@
 package utils;
 
+import static org.testng.Assert.assertEquals;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+
+import values.values;
+
 
 public class webdriverfunctions {
 
 	protected WebDriver driver ; 
-	
+	values val= new values();
 	
     public String typebyxpath(String loc, String val){
     	driver.findElement(By.xpath(loc)).clear();
@@ -18,6 +23,19 @@ public class webdriverfunctions {
 	
 	public boolean displayverification (String loc){return driver.findElement(By.xpath(loc)).isDisplayed();}
 	
-	public String textverification (String loc){return driver.findElement(By.xpath(loc)).getAttribute("value");}
+	public String textverification (String loc)
+	{return driver.findElement(By.xpath(loc)).getAttribute("value");}
 
+	public boolean txtverify(String loc, String value){
+		try{
+			assertEquals(Integer.parseInt(driver.findElement(By.xpath(loc)).getAttribute("value")), val);
+		
+		return true;
+	
+		}
+		catch(Throwable t){
+			System.out.println(t.getMessage());
+			return false;
+		}
+	}
 }
